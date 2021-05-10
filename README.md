ee05bcfd4334ea4a227400dd6d724fdd3f8b22e9
Scenaristeur <scenrister@galcm>
2021-05-10 13:17:39 +0200

    init, see vatcher for the server

---
+++ README.md
@@ -0,0 +1,3 @@
+# chat socket.io vuejs
+
+https://medium.com/js-dojo/build-a-real-time-chat-app-with-vuejs-socket-io-and-nodejs-714c8eefa54e
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button @click="state.count++">count is: {{ state.count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+
+
+  socket.on('init', function(init) {
+    console.log(init)
+    // pathsep = init.pathsep
+    // let item = document.createElement('li');
+    // item.textContent = init.welcome;
+    // messages.appendChild(item);
+    //  window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  socket.on('chat message', function(msg) {
+    console.log(msg)
+    // let item = document.createElement('li');
+    // item.textContent = msg;
+    // messages.appendChild(item);
+    // window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  socket.on('watcher event', function(msg) {
+    console.log(msg)
+    // process(msg)
+    // let item = document.createElement('li');
+    // item.textContent = JSON.stringify(msg);
+    // messages.appendChild(item);
+    //  window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  socket.on('cat file', function(msg) {
+    console.log(msg)
+    // let item = document.createElement('li');
+    // item.textContent = msg;
+    // messages.appendChild(item);
+    // //  window.scrollTo(0, document.body.scrollHeight);
+    //
+    // processFile(msg)
+  });
+
+  socket.on('connect', () => {
+    console.log('user connected');
+  //  document.body.style.backgroundColor = "rgba(11,156,49,0.1)"
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  //  document.body.style.backgroundColor = "rgba(156,11,49,0.1)"
+  });
+
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+console.log("Socket",socket)
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
