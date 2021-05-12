18d2838561ea25886c4266226aec9237ee330569
Scenaristeur <scenrister@galcm>
2021-05-12 12:35:11 +0200

    buildpath

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const publicPath = process.env.NODE_ENV === 'production' ? '/vatch-vite/' : '/'
+const base = process.env.NODE_ENV === 'production' ? '/vatch-vite/' : '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: base,
   publicPath: publicPath,
 })
