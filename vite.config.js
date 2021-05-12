404a7fbf4cf8c81abb5a27b7dc8c8f594e20eda2
Scenaristeur <scenrister@galcm>
2021-05-12 12:32:11 +0200

    buildpath

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const publicPath = process.env.NODE_ENV === 'production' ? '/bon-prix/' : '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  publicPath: publicPath,
 })
